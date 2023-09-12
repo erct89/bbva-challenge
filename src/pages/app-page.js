140fcdbb8bbc48935896b496695917caf9dee668
Emilio AG <emilio_rct@hotmail.es>
2023-09-12 02:07:11 +0200

    feat(app-page): add router

--- src/pages/app-page.js
+++ src/pages/app-page.js
@@ -1,9 +1,16 @@
 import { LitElement, html, css } from 'lit';
+import { Router } from '@lit-labs/router';
+
+import './components/data-providers/storage-data-provider.js';
 
 import './home-page.js';
 import './game-page.js';
 import './scores-page.js';
 import './rules-page.js';
+
+import {
+  LOCAL_STORAGE_KEY
+} from './constants.js';
 
 class AppPage extends LitElement {
   static get is() {
@@ -11,23 +18,85 @@ class AppPage extends LitElement {
   }
 
   static get properties() {
-    return {};
+    return {
+      _router: { type: Object },
+      _storageDP: { type: Object },
+    };
   }
 
   static get styles() {
     return css``;
   }
 
-  // constructor() {
-  //   super();
-  // }
-
-  static get _showHeader() {
-    return true;
+  constructor() {
+    super();
+    this._router = new Router(this, this.routes);
+    this._storageDP = {};
   }
 
-  static get _links() {
-    return [{ path: '/', label: 'Home' }];
+  firstUpdated() {
+    super.firstUpdated();
+
+    this._storageDP = this.shadowRoot.querySelector('#storageDP');
+  }
+
+  get routes() {
+    return [
+      {
+        path: '/',
+        render: () => html`<page-template>
+          <home-page></home-page>
+        </page-template>`,
+      },
+      {
+        path: '/game',
+        label: 'Memoriy',
+        render: () => html`
+          <page-template show-header show-nav>
+            ${this._renderHeader}
+            <game-page></game-page>
+          </page-template>
+        `,
+        enter: () => this._authenticationRoute(),
+      },
+      {
+        path: '/scores',
+        label: 'Scores',
+        render: () => html` <page-template show-header show-nav>
+          ${this._renderHeader}
+
+          <scores-page></scores-page>
+        </page-template>`,
+        enter: () => this._authenticationRoute(),
+      },
+      {
+        path: '/rules',
+        label: 'Rules',
+        render: () => html`<page-template show-header show-nav>
+          ${this._renderHeader}
+          <rules-page></rules-page>
+        </page-template>`,
+        enter: () => this._authenticationRoute(),
+      },
+      {
+        path: '/config',
+        label: 'Configs',
+        render: () => html`<page-template show-header show-nav>
+          ${this._renderHeader}
+          <main>
+            <config-page> </config-page>
+          </main>
+        </page-template>`,
+        enter: () => this._authenticationRoute(),
+      },
+      {
+        path: '/*',
+        enter: () => {
+          this._redirect('/');
+          return false;
+        },
+      },
+    ];
   }
 
   get _renderHeader() {
@@ -40,14 +109,8 @@ class AppPage extends LitElement {
 
   render() {
     return html`<div class="app">
-      <page-template
-        ?show-header="${this._showHeader}"
-        ?show-nav="${this._showHeader}"
-        .links="${this._links}"
-      >
-        ${this._renderHeader}
-        <main>App PAGE</main>
-      </page-template>
+      <storage-data-provider id="storageDP" host="${LOCAL_STORAGE_KEY}"></storage-data-provider>
+      <main>${this._router.outlet()}</main>
     </div>`;
   }
 }
