d28980da6a6f6f5a1ce8327b65d3bf360c349ddf
Emilio AG <emilio_rct@hotmail.es>
2023-09-13 23:11:04 +0200

    feat(): add pwa button install

--- src/pages/app-page.js
+++ src/pages/app-page.js
@@ -1,13 +1,23 @@
-import { LitElement, html, css } from 'lit';
+/* eslint-disable class-methods-use-this */
+import { LitElement, html, css, nothing } from 'lit';
+import { classMap } from 'lit/directives/class-map.js';
 import { Router } from '@lit-labs/router';
 
+// Data Providers
 import '../components/data-providers/storage-data-provider.js';
+import '../components/input-button.js';
 
+// views
+import 'pwa-helper-components/pwa-install-button.js';
+import 'pwa-helper-components/pwa-update-available.js';
 import '../components/page-template.js';
 import './home-page.js';
 import './game-page.js';
 import './scores-page.js';
 import './rules-page.js';
+import './config-page.js';
+
+import styles from './app-page-styles.js';
 
 import {
   LOCAL_STORAGE_KEY,
@@ -22,33 +32,21 @@ class AppPage extends LitElement {
     return 'app-page';
   }
 
-  static get properties() {
-    return {
-      _appHeader: { type: String },
-      _appConfig: { type: Object },
-      _levels: { type: Array },
-      _numberCardsToFind: { type: Array },
-      _router: { type: Object },
-      _scores: { type: Array },
-      _storageDP: { type: Object },
-      _user: { type: String },
-      _userScores: { type: Array },
-    };
-  }
+  static properties = {
+    _appHeader: { type: String },
+    _appConfig: { type: Object },
+    _levels: { type: Array },
+    _numberCardsToFind: { type: Array },
+    _router: { type: Object },
+    _scores: { type: Array },
+    _user: { type: String },
+    _userScores: { type: Array },
+    _storageDP: { type: Object },
+    _notifyMessage: { type: String, status: true },
+  };
 
   static get styles() {
-    return css`
-      .content {
-        width: 100%;
-        height: 100%;
-      }
-
-      .content--center {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-    `;
+    return [styles];
   }
 
   constructor() {
@@ -58,13 +56,14 @@ class AppPage extends LitElement {
       numberCardsToFind: DEFAULT_NUMBER_CARDS_TO_FIND,
       level: DEFAULT_LEVEL,
     };
-    this._levels = LEVELS;
-    this._numberCardsToFind = NUMBER_CARDS_TO_FIND;
     this._router = new Router(this, this.routes);
     this._scores = [];
-    this._storageDP = {};
     this._user = '';
     this._userScores = [];
+    this._levels = LEVELS;
+    this._numberCardsToFind = NUMBER_CARDS_TO_FIND;
+    this._storageDP = {};
+    this._notifyMessage = '';
   }
 
   firstUpdated() {
@@ -78,67 +77,80 @@ class AppPage extends LitElement {
     return [
       {
         path: '/',
-        render: () => html`<page-template>
+        render: () => html`<page-template show-footer>
           <home-page
             class="content content--center"
             app-header="${this._appHeader}"
             user="${this._user}"
             @user-name-changed="${this._registerUser}"
-          >
-          </home-page>
+          ></home-page>
+          ${this._renderFooter}
         </page-template>`,
       },
       {
         path: '/game',
         label: 'Game',
-        enter: () => this._authenticationRoute(),
         render: () => html`
-          <page-template show-header show-nav .links="${this._links('/game')}">
+          <page-template
+            show-header
+            show-nav
+            show-footer
+            .links="${this._links('/game')}"
+          >
             ${this._renderHeader}
             <game-page
-              class="content content--center"
               .config="${this._appConfig}"
               @game-over="${this._registerScore}"
             ></game-page>
+            ${this._renderFooter}
           </page-template>
         `,
+        enter: () => this._authenticationRoute(),
       },
       {
         path: '/scores',
         label: 'Scores',
-        enter: () => this._authenticationRoute(),
         render: () => html` <page-template
           show-header
           show-nav
+          show-footer
           .links="${this._links('/scores')}"
         >
           ${this._renderHeader}
+
           <scores-page
             .scores="${this._scores}"
             .userScores=${this._userScores}
           ></scores-page>
+
+          ${this._renderFooter}
         </page-template>`,
+        enter: () => this._authenticationRoute(),
       },
       {
         path: '/rules',
         label: 'Rules',
-        enter: () => this._authenticationRoute(),
         render: () => html`<page-template
           show-header
           show-nav
+          show-footer
           .links="${this._links('/rules')}"
         >
           ${this._renderHeader}
+
           <rules-page></rules-page>
+
+          ${this._renderFooter}
         </page-template>`,
+        enter: () => this._authenticationRoute(),
       },
       {
         path: '/config',
         label: 'Configs',
-        enter: () => this._authenticationRoute(),
         render: () => html`<page-template
           show-header
           show-nav
+          show-footer
           .links="${this._links('/rules')}"
         >
           ${this._renderHeader}
@@ -149,9 +161,13 @@ class AppPage extends LitElement {
                 levels: this._levels,
                 optionsNumbersCardsToFind: this._numberCardsToFind,
               }}"
-            ></config-page>
+              @configs-change="${this._registerConfig}"
+            >
+            </config-page>
           </main>
+          ${this._renderFooter}
         </page-template>`,
+        enter: () => this._authenticationRoute(),
       },
       {
         path: '/*',
@@ -163,9 +179,12 @@ class AppPage extends LitElement {
     ];
   }
 
-
   get _isLogin() {
     return !!this._user;
+  }
+
+  _redirect(path) {
+    this._router.goto(path);
   }
 
   _authenticationRoute() {
@@ -178,14 +197,6 @@ class AppPage extends LitElement {
     return result;
   }
 
-  _links(page) {
-    return this.routes.filter(({ path, label }) => label && page !== path);
-  }
-
-  _redirect(path) {
-    this._router.goto(path);
-  }
-
   _registerUser({ detail }) {
     const { appConfig, user, userScores } =
       this._storageDP.registerUser(detail);
@@ -195,6 +206,7 @@ class AppPage extends LitElement {
     this._userScores = userScores || this._userScores;
 
     this._redirect('/game');
+    this._pushNotify('Register User');
   }
 
   _registerScore({ detail }) {
@@ -204,14 +216,27 @@ class AppPage extends LitElement {
 
     this._scores = this._storageDP.getScores(false, []);
     this._userScores = this._storageDP.getScores(this._user, []);
+    this._pushNotify('Score Save!!');
   }
 
   _registerConfig({ detail }) {
     const { level, numberCardsToFind } = detail;
     this._appConfig = { level, numberCardsToFind };
     this._storageDP.saveConfig(this._user, this._appConfig);
+    this._pushNotify('New Config Save!!');
   }
 
+  _pushNotify(msg) {
+    this._notifyMessage = msg;
+
+    window.setTimeout(() => {
+      this._notifyMessage = '';
+    }, 1500);
+  }
+
+  _links(page) {
+    return this.routes.filter(({ path, label }) => label && page !== path);
+  }
 
   get _renderHeader() {
     return html`<div class="content content--center" slot="header-left">
@@ -227,14 +252,34 @@ class AppPage extends LitElement {
       </div>`;
   }
 
-  render() {
-    return html`<div class="app">
-      <storage-data-provider
-        id="storageDP"
-        host="${LOCAL_STORAGE_KEY}"
-      ></storage-data-provider>
-      <main>${this._router.outlet()}</main>
+  get _renderFooter() {
+    return html`<div slot="footer-content">
+      <pwa-install-button>
+        <input-button label="Install App" class="info small"></input-button>
+      </pwa-install-button>
+      <pwa-update-available>
+        <input-button label="Update App" class="info small"></input-button>
+      </pwa-update-available>
     </div>`;
+  }
+
+  render() {
+    const notifyClasses = classMap({ 'notify-show': this._notifyMessage });
+
+    return html`
+      <div class="app">
+        <storage-data-provider
+          id="storageDP"
+          host="${LOCAL_STORAGE_KEY}"
+        ></storage-data-provider>
+        ${this._router.outlet()}
+        ${this._notifyMessage
+          ? html`<div class="${notifyClasses}">
+              <p>${this._notifyMessage}</p>
+            </div>`
+          : nothing}
+      </div>
+    `;
   }
 }
 
